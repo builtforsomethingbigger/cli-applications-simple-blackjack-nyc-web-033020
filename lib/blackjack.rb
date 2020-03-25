9347c8917d9c9a2a254dd52e920669c0f02d8c52
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-25 02:08:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,12 +55,12 @@ end
 #####################################################
 
 def runner
-  card_total = initial_round
-   until card_total > 21
-     card_total = hit?(card_total)
-     display_card_total(card_total)
-   end
-   end_game(card_total)
+  # code runner here	  welcome
+ card_total = initial_round
+ until card_total > 21
+   card_total = hit?(card_total)
+   display_card_total(card_total)
+ end
+ end_game(card_total)
 
-  end
-end
+end	
